d6ad88157b1532f8193b762707869c73b5f124f5
Tim VanReenen <tim.vanreenen@gmail.com>
2026-03-10 11:01:40 -0400

    Shorten UTC offset a bit

--- sketchybar/.config/sketchybar/plugins/time.sh
+++ sketchybar/.config/sketchybar/plugins/time.sh
@@ -3,7 +3,8 @@
 # Format: DATE|WEEK|DAY_ABBR|TIME|OFFSET
 LABEL=$(date '+%Y-%m-%d|%V|%a|%H:%M:%S|%z' | awk -F'|' '{
   week = int($2)
-  printf "%s W%d %s %s %s", $1, week, substr($3, 1, 1), $4, $5
+  tz = sprintf("%+.1g", (substr($5,1,1)=="-"?-1:1) * (substr($5,2,2) + substr($5,4,2)/60))
+  printf "%s W%d %s %s %s", $1, week, substr($3, 1, 1), $4, tz
 }')
 
 sketchybar --set "$NAME" label="$LABEL"
